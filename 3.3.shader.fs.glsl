456c07fb4832c2299e9ac815c745983b53fa4652
shen wei <534965993@qq.com>
2023-05-18 18:58:33 +0800

    day01

--- 3.3.shader.fs.glsl
+++ 3.3.shader.fs.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourPosition;
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture0;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(ourPosition, 1.0f);
+    FragColor = mix(texture(texture0, TexCoord), texture(texture2, TexCoord), 0.2);
 }
