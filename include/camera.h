3324db30420227ece96c51d818e291f7b5c6e2ef
shen wei <534965993@qq.com>
2023-05-22 12:41:04 +0800

    light cast

--- include/camera.h
+++ include/camera.h
@@ -109,8 +109,8 @@ public:
         Zoom -= (float)yoffset;
         if (Zoom < 1.0f)
             Zoom = 1.0f;
-        if (Zoom > 89.0f)
-            Zoom = 89.0f;
+        if (Zoom > 45.0f)
+            Zoom = 45.0f;
     }
 
 private:
